a7e2045cb05ee74245926436c4aceb34673cea2e
Zbigniew Baranski <zb.baranski@icloud.com>
2019-10-01 21:10:22 +0200

    add files

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <header>This is a placeholder for a header</header>
+        </div>
+    )
+};
+
+export default Header;
---
+++ src/components/MainContent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MainContent() {
+    return (
+        <main> There will be a main content rendered here.</main>
+    )
+}
+
+export default MainContent;
